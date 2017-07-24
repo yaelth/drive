yaelth/drive

commit 0d1e5f8ce6ffed0f340cd806a7e58a9bf9becff9

propagate and handle remote file resolution errors
Fixes https://github.com/odeke-em/drive/issues/480.
Fixes https://github.com/odeke-em/drive/issues/668.
Fixes https://github.com/odeke-em/drive/issues/728.
Fixes https://github.com/odeke-em/drive/issues/738.

Ensure that we propagate any remote file resolution errors
during pagination.
Previously pagination was done in a goroutine that just sent
back a channel of files. However, this design meant that
if errors were encountered during the resolution, the channel
would close after just
```go
if err != nil {
  fmt.Println(err)
  break
}
```

and then close the channel.
Meanwhile the problem then bubbled up to the resolver
that determined which files exist and which don't
thereby triggering deletion operations claiming the remote
files didn't exist say when we Google Drive encountered a 500 or a 504.

This CL fixes these problems by ensuring that we return a paginationPair
which contains two channels:
a) a channel for encountered errors
b) a channel for the actual files as was before.

This then translates to consumers of remote file pagination have to
multiplex on both channels with a select loop and watching if any
errors came through and then propagate that along to the callers
to avoid making a simple bad situation, a total disaster.

* Exhibits:

To demonstrate the problems with the old and new behavior, let's make a
simulated remote error after resolving a few files, the typical would
be a 500 or 504 from Google Drive.
As you'll see, the old behavior would report a new for a total
addition/deletion which is wrong/faulty behavior causing duplicates
for additions, and spurious additions on remote/local, but also
worse causing deletion of files.

In the new behavior, those problems are trivially handled which is
the correct behavior.

+ Before
```diff
diff --git a/src/remote.go b/src/remote.go
index 1219915..6edbf1a 100644
--- a/src/remote.go
+++ b/src/remote.go

src/remote.go
@@ -71,6 +71,8 @@ var (
 		CLIOptionIgnoreNameClashes, CLIOptionFixClashesKey))
 	ErrClashFixingAborted             = clashFixingAbortedErr(fmt.Errorf("clash fixing aborted"))
 	ErrGoogleAPIInvalidQueryHardCoded = invalidGoogleAPIQueryErr(fmt.Errorf("GoogleAPI: Error 400: Invalid query, invalid"))
+
+	errNilParent = nonExistantRemoteErr(fmt.Errorf("remote parent doesn't exist"))
 )
 
 var (
@@ -211,20 +213,26 @@ func (r *Remote) FindBackPaths(id string) (backPaths []string, err error) {
 	return
 }
 
-func wrapInChan(f *File, err error) chan *File {
-	ch := make(chan *File)
+func wrapInPaginationPair(f *File, err error) *paginationPair {
+	fChan := make(chan *File)
+	errsChan := make(chan error)
 
 	go func() {
-		defer close(ch)
-		ch <- f
+		defer close(fChan)
+		fChan <- f
 	}()
 
-	return ch
+	go func() {
+		defer close(errsChan)
+		errsChan <- err
+	}()
+
+	return &paginationPair{errsChan: errsChan, filesChan: fChan}
 }
 
-func (r *Remote) FindByIdM(id string) chan *File {
+func (r *Remote) FindByIdM(id string) *paginationPair {
 	f, err := r.FindById(id)
-	return wrapInChan(f, err)
+	return wrapInPaginationPair(f, err)
 }
 
 func (r *Remote) FindById(id string) (*File, error) {
@@ -249,7 +257,7 @@ func retryableChangeOp(fn func() (interface{}, error), debug bool, retryCount in
 	}
 }
 
-func (r *Remote) findByPathM(p string, trashed bool) chan *File {
+func (r *Remote) findByPathM(p string, trashed bool) *paginationPair {
 	if rootLike(p) {
 		return r.FindByIdM("root")
 	}
@@ -279,7 +287,7 @@ func (r *Remote) FindByPath(p string) (*File, error) {
 	return r.findByPath(p, false)
 }
 
-func (r *Remote) FindByPathM(p string) chan *File {
+func (r *Remote) FindByPathM(p string) *paginationPair {
 	return r.findByPathM(p, false)
 }
 
@@ -287,21 +295,30 @@ func (r *Remote) FindByPathTrashed(p string) (*File, error) {
 	return r.findByPath(p, true)
 }
 
-func (r *Remote) FindByPathTrashedM(p string) chan *File {
+func (r *Remote) FindByPathTrashedM(p string) *paginationPair {
 	return r.findByPathM(p, true)
 }
 
-func reqDoPage(req *drive.FilesListCall, hidden bool, promptOnPagination bool) chan *File {
+func reqDoPage(req *drive.FilesListCall, hidden bool, promptOnPagination bool) *paginationPair {
 	return _reqDoPage(req, hidden, promptOnPagination, false)
 }
 
-func _reqDoPage(req *drive.FilesListCall, hidden bool, promptOnPagination, nilOnNoMatch bool) chan *File {
-	fileChan := make(chan *File)
+type paginationPair struct {
+	errsChan  chan error
+	filesChan chan *File
+}
+
+func _reqDoPage(req *drive.FilesListCall, hidden bool, promptOnPagination, nilOnNoMatch bool) *paginationPair {
+	filesChan := make(chan *File)
+	errsChan := make(chan error)
 
 	throttle := time.Tick(1e7)
 
 	go func() {
-		defer close(fileChan)
+		defer func() {
+			close(errsChan)
+			close(filesChan)
+		}()
 
 		pageToken := ""
 		for pageIterCount := uint64(0); ; pageIterCount++ {
@@ -310,7 +327,7 @@ func _reqDoPage(req *drive.FilesListCall, hidden bool, promptOnPagination, nilOn
 			}
 			results, err := req.Do()
 			if err != nil {
-				fmt.Println(err)
+				errsChan <- err
 				break
 			}
 
@@ -320,13 +337,14 @@ func _reqDoPage(req *drive.FilesListCall, hidden bool, promptOnPagination, nilOn
 					continue
 				}
 				iterCount += 1
-				fileChan <- NewRemoteFile(f)
+				filesChan <- NewRemoteFile(f)
 			}
+
 			pageToken = results.NextPageToken
 			if pageToken == "" {
 				if nilOnNoMatch && len(results.Items) < 1 && pageIterCount < 1 {
 					// Item absolutely doesn't exist
-					fileChan <- nil
+					filesChan <- nil
 				}
 				break
 			}
@@ -337,26 +355,26 @@ func _reqDoPage(req *drive.FilesListCall, hidden bool, promptOnPagination, nilOn
 			}
 
 			if promptOnPagination && !nextPage() {
-				fileChan <- nil
+				filesChan <- nil
 				break
 			}
 		}
 	}()
 
-	return fileChan
+	return &paginationPair{filesChan: filesChan, errsChan: errsChan}
 }
 
-func (r *Remote) findByParentIdRaw(parentId string, trashed, hidden bool) chan *File {
+func (r *Remote) findByParentIdRaw(parentId string, trashed, hidden bool) *paginationPair {
 	req := r.service.Files.List()
 	req.Q(fmt.Sprintf("%s in parents and trashed=%v", customQuote(parentId), trashed))
 	return reqDoPage(req, hidden, false)
 }
 
-func (r *Remote) FindByParentId(parentId string, hidden bool) chan *File {
+func (r *Remote) FindByParentId(parentId string, hidden bool) *paginationPair {
 	return r.findByParentIdRaw(parentId, false, hidden)
 }
 
-func (r *Remote) FindByParentIdTrashed(parentId string, hidden bool) chan *File {
+func (r *Remote) FindByParentIdTrashed(parentId string, hidden bool) *paginationPair {
 	return r.findByParentIdRaw(parentId, true, hidden)
 }
 
@@ -936,7 +954,7 @@ func (r *Remote) UpsertByComparison(args *upsertOpt) (f *File, err error) {
 	return f, err
 }
 
-func (r *Remote) findShared(p []string) (chan *File, error) {
+func (r *Remote) findShared(p []string) *paginationPair {
 	req := r.service.Files.List()
 	expr := "sharedWithMe=true"
 	if len(p) >= 1 {
@@ -944,10 +962,10 @@ func (r *Remote) findShared(p []string) (chan *File, error) {
 	}
 	req = req.Q(expr)
 
-	return reqDoPage(req, false, false), nil
+	return reqDoPage(req, false, false)
 }
 
-func (r *Remote) FindByPathShared(p string) (chan *File, error) {
+func (r *Remote) FindByPathShared(p string) *paginationPair {
 	if p == "/" || p == "root" {
 		return r.findShared([]string{})
 	}
@@ -964,19 +982,20 @@ func (r *Remote) FindByPathShared(p string) (chan *File, error) {
 	return r.findShared(nonEmpty)
 }
 
-func (r *Remote) FindStarred(trashed, hidden bool) (chan *File, error) {
+func (r *Remote) FindStarred(trashed, hidden bool) *paginationPair {
 	req := r.service.Files.List()
 	expr := fmt.Sprintf("(starred=true) and (trashed=%v)", trashed)
 	req.Q(expr)
-	return reqDoPage(req, hidden, false), nil
+	return reqDoPage(req, hidden, false)
 }
 
-func (r *Remote) FindMatches(mq *matchQuery) (chan *File, error) {
+func (r *Remote) FindMatches(mq *matchQuery) *paginationPair {
 	parent, err := r.FindByPath(mq.dirPath)
-	filesChan := make(chan *File)
 	if err != nil || parent == nil {
-		close(filesChan)
-		return filesChan, err
+		if parent == nil && err == nil {
+			err = errNilParent
+		}
+		return wrapInPaginationPair(parent, err)
 	}
 
 	req := r.service.Files.List()
@@ -985,10 +1004,10 @@ func (r *Remote) FindMatches(mq *matchQuery) (chan *File, error) {
 	expr := sepJoinNonEmpty(" and ", parQuery, mq.Stringer())
 
 	req.Q(expr)
-	return reqDoPage(req, true, false), nil
+	return reqDoPage(req, true, false)
 }
 
-func (r *Remote) findChildren(parentId string, trashed bool) chan *File {
+func (r *Remote) findChildren(parentId string, trashed bool) *paginationPair {
 	req := r.service.Files.List()
 	req.Q(fmt.Sprintf("%s in parents and trashed=%v", customQuote(parentId), trashed))
 	return reqDoPage(req, true, false)
@@ -998,10 +1017,11 @@ func (r *Remote) About() (*drive.About, error) {
 	return r.service.About.Get().Do()
 }
 
-func (r *Remote) findByPathRecvRawM(parentId string, p []string, trashed bool) chan *File {
-	chanOChan := make(chan chan *File)
+func (r *Remote) findByPathRecvRawM(parentId string, p []string, trashed bool) *paginationPair {
+	chanOChan := make(chan *paginationPair)
 
-	resolvedResults := make(chan *File)
+	resolvedFilesChan := make(chan *File)
+	resolvedErrsChan := make(chan error)
 
 	go func() {
 		defer close(chanOChan)
@@ -1024,44 +1044,68 @@ func (r *Remote) findByPathRecvRawM(parentId string, p []string, trashed bool) c
 		}
 
 		req.Q(expr)
-		resultsChan := _reqDoPage(req, true, false, true)
+		pager := _reqDoPage(req, true, false, true)
 
 		if len(rest) < 1 {
-			chanOChan <- resultsChan
+			chanOChan <- pager
 			return
 		}
 
-		/*
-			files, err := req.Do()
-			if err != nil {
-			    if err.Error() == ErrGoogleAPIInvalidQueryHardCoded.Error() { // Send the user back the query information
-				err = fmt.Errorf("err: %v query: `%s`", err, expr)
-			    }
+		resultsChan := pager.filesChan
+		errsChan := pager.errsChan
 
-			    return nil, err
+		working := true
+		for working {
+			select {
+			case err := <-errsChan:
+				if err != nil {
+					chanOChan <- wrapInPaginationPair(nil, err)
+				}
+			case f, stillHasContent := <-resultsChan:
+				if !stillHasContent {
+					working = false
+					break
+				}
+				if f != nil {
+					chanOChan <- r.findByPathRecvRawM(f.Id, rest, trashed)
+				}
 			}
-		*/
-		for f := range resultsChan {
-			if f == nil {
-				resolvedResults <- f
-				continue
-			}
-
-			chanOChan <- r.findByPathRecvRawM(f.Id, rest, trashed)
 		}
 	}()
 
 	go func() {
-		defer close(resolvedResults)
+		defer func() {
+			close(resolvedFilesChan)
+			close(resolvedErrsChan)
+		}()
 
-		for ch := range chanOChan {
-			for f := range ch {
-				resolvedResults <- f
+		for curPagePair := range chanOChan {
+			if curPagePair == nil {
+				continue
+			}
+
+			errsChan := curPagePair.errsChan
+			filesChan := curPagePair.filesChan
+
+			working := true
+			for working {
+				select {
+				case err := <-errsChan:
+					if err != nil {
+						resolvedErrsChan <- err
+					}
+				case f, stillHasContent := <-filesChan:
+					if !stillHasContent {
+						working = false
+						break
+					}
+					resolvedFilesChan <- f
+				}
 			}
 		}
 	}()
 
-	return resolvedResults
+	return &paginationPair{errsChan: resolvedErrsChan, filesChan: resolvedFilesChan}
 }
 
 func (r *Remote) findByPathRecvRaw(parentId string, p []string, trashed bool) (*File, error) {
@@ -1105,11 +1149,11 @@ func (r *Remote) findByPathRecv(parentId string, p []string) (*File, error) {
 	return r.findByPathRecvRaw(parentId, p, false)
 }
 
-func (r *Remote) findByPathRecvM(parentId string, p []string) chan *File {
+func (r *Remote) findByPathRecvM(parentId string, p []string) *paginationPair {
 	return r.findByPathRecvRawM(parentId, p, false)
 }
 
-func (r *Remote) findByPathTrashedM(parentId string, p []string) chan *File {
+func (r *Remote) findByPathTrashedM(parentId string, p []string) *paginationPair {
 	return r.findByPathRecvRawM(parentId, p, true)
 }
 

src/changes.go
@@ -141,25 +141,40 @@ func (g *Commands) byRemoteResolve(relToRoot, fsPath string, r *File, push bool)
 }
 
 func (g *Commands) changeListResolve(relToRoot, fsPath string, push bool) (cl, clashes []*Change, err error) {
-
-	remotesChan := g.rem.FindByPathM(relToRoot)
+	pagePair := g.rem.FindByPathM(relToRoot)
 	iterCount := uint64(0)
 	noClashThreshold := uint64(1)
 
-	for rem := range remotesChan {
-		if rem != nil {
-			if anyMatch(g.opts.Ignorer, rem.Name) {
-				return
+	errsChan := pagePair.errsChan
+	remotesChan := pagePair.filesChan
+
+	working := true
+	for working {
+		select {
+		case rErr := <-errsChan:
+			if rErr != nil {
+				return nil, nil, rErr
 			}
-			iterCount++
-		}
+		case rem, stillHasContent := <-remotesChan:
+			if !stillHasContent {
+				working = false
+				break
+			}
 
-		ccl, cclashes, cErr := g.byRemoteResolve(relToRoot, fsPath, rem, push)
+			if rem != nil {
+				if anyMatch(g.opts.Ignorer, rem.Name) {
+					return
+				}
+				iterCount++
+			}
 
-		cl = append(cl, ccl...)
-		clashes = append(clashes, cclashes...)
-		if cErr != nil {
-			err = combineErrors(err, cErr)
+			ccl, cclashes, cErr := g.byRemoteResolve(relToRoot, fsPath, rem, push)
+
+			cl = append(cl, ccl...)
+			clashes = append(clashes, cclashes...)
+			if cErr != nil {
+				err = combineErrors(err, cErr)
+			}
 		}
 	}
 
@@ -394,14 +409,26 @@ func (g *Commands) resolveChangeListRecv(clr *changeListResolve) (cl, clashes []
 		}
 	}
 
-	var remoteChildren chan *File
+	var pagePair *paginationPair
+
 	if r != nil {
-		remoteChildren = g.rem.FindByParentId(r.Id, g.opts.Hidden)
+		pagePair = g.rem.FindByParentId(r.Id, g.opts.Hidden)
 	} else {
-		remoteChildren = make(chan *File)
-		close(remoteChildren)
+		// TODO: Figure out if the condition
+		// file == nil && err == nil
+		// is an inconsitent state.
+		errsChan := make(chan error)
+		go close(errsChan)
+		filesChan := make(chan *File)
+		go close(filesChan)
+
+		pagePair = &paginationPair{errsChan: errsChan, filesChan: filesChan}
 	}
-	dirlist, clashingFiles := merge(remoteChildren, localChildren, g.opts.IgnoreNameClashes)
+
+	dirlist, clashingFiles, err := merge(pagePair, localChildren, g.opts.IgnoreNameClashes)
+	if err != nil {
+		return nil, nil, err
+	}
 
 	if !g.opts.IgnoreNameClashes && len(clashingFiles) >= 1 {
 		remoteBase := clr.remoteBase
@@ -523,7 +550,7 @@ func (g *Commands) changeSlice(cslArg *changeSliceArg) {
 	}
 }
 
-func merge(remotes, locals chan *File, ignoreClashes bool) (merged []*dirList, clashes []*File) {
+func merge(remotePagePair *paginationPair, locals chan *File, ignoreClashes bool) (merged []*dirList, clashes []*File, err error) {
 	localsMap := map[string]*File{}
 	remotesMap := map[string]*File{}
 
@@ -546,27 +573,49 @@ func merge(remotes, locals chan *File, ignoreClashes bool) (merged []*dirList, c
 		localsMap[l.Name] = l
 	}
 
-	for r := range remotes {
-		list := &dirList{remote: r}
+	working := true
+	for working {
+		select {
+		case err := <-remotePagePair.errsChan:
+			// It is imperative that as soon as we catch an error
+			// from the remote, let's immediately return
+			// otherwise we'll be reporting false results.
+			// See Issues:
+			//   https://github.com/odeke-em/drive/issues/480
+			//   https://github.com/odeke-em/drive/issues/668
+			//   https://github.com/odeke-em/drive/issues/728
+			//   https://github.com/odeke-em/drive/issues/738
+			// and a multitude of other issues that were caused by error responses
+			// from remote falsely being translated as "the file doesn't exist"
+			if err != nil {
+				return merged, clashes, err
+			}
+		case r, stillHasContent := <-remotePagePair.filesChan:
+			if !stillHasContent {
+				working = false
+				break
+			}
+			list := &dirList{remote: r}
 
-		if !ignoreClashes {
-			prev, present := remotesMap[r.Name]
-			if present {
-				registerClash(r)
-				registerClash(prev)
-				continue
+			if !ignoreClashes {
+				prev, present := remotesMap[r.Name]
+				if present {
+					registerClash(r)
+					registerClash(prev)
+					continue
+				}
+
+				remotesMap[r.Name] = r
 			}
 
-			remotesMap[r.Name] = r
+			l, ok := localsMap[r.Name]
+			// look for local
+			if ok && l != nil && l.IsDir == r.IsDir {
+				list.local = l
+				delete(localsMap, r.Name)
+			}
+			merged = append(merged, list)
 		}
-
-		l, ok := localsMap[r.Name]
-		// look for local
-		if ok && l != nil && l.IsDir == r.IsDir {
-			list.local = l
-			delete(localsMap, r.Name)
-		}
-		merged = append(merged, list)
 	}
 
 	// if anything left in locals, add to the dir listing

src/clashes.go
@@ -65,9 +65,10 @@ func findClashesForChildren(g *Commands, parentId, relToRootPath string, depth i
 	}
 
 	memoized := map[string][]*File{}
-	children := g.rem.FindByParentId(parentId, g.opts.Hidden)
+	pagePair := g.rem.FindByParentId(parentId, g.opts.Hidden)
 	decrementedDepth := decrementTraversalDepth(depth)
 
+	children := pagePair.filesChan
 	discoveryOrder := []string{}
 	for child := range children {
 		if child == nil {
@@ -80,6 +81,14 @@ func findClashesForChildren(g *Commands, parentId, relToRootPath string, depth i
 
 		cluster = append(cluster, child)
 		memoized[child.Name] = cluster
+	}
+
+	// Now ensure that no error ensued during pagination/retrieval
+	for rErr := range pagePair.errsChan {
+		if rErr != nil {
+			err = rErr
+			return clashes, err
+		}
 	}
 
 	separatorPrefix := relToRootPath
@@ -121,31 +130,51 @@ func findClashesByPath(g *Commands, relToRootPath string, depth int) (clashes []
 		return
 	}
 
-	remotes := g.rem.FindByPathM(relToRootPath)
-
 	iterCount := uint64(0)
 	clashThresholdCount := uint64(1)
 
 	cl := []*Change{}
 
-	for rem := range remotes {
-		if rem == nil {
-			continue
+	pagePair := g.rem.FindByPathM(relToRootPath)
+
+	working := true
+	for working {
+		select {
+		case err := <-pagePair.errsChan:
+			if err != nil {
+				return clashes, err
+			}
+		case rem, stillHasContent := <-pagePair.filesChan:
+			if !stillHasContent {
+				working = false
+				break
+			}
+			if rem == nil {
+				continue
+			}
+
+			iterCount++
+
+			change := &Change{
+				Src: rem, g: g,
+				Path:   relToRootPath,
+				Parent: g.opts.Path,
+			}
+
+			cl = append(cl, change)
 		}
-
-		iterCount++
-
-		change := &Change{
-			Src: rem, g: g,
-			Path:   relToRootPath,
-			Parent: g.opts.Path,
-		}
-
-		cl = append(cl, change)
 	}
 
 	if iterCount > clashThresholdCount {
 		clashes = append(clashes, cl...)
+	}
+
+	// Check for any errors that were
+	// encountered during remote file retrieval
+	for pageErr := range pagePair.errsChan {
+		if pageErr == nil {
+			return clashes, pageErr
+		}
 	}
 
 	decrementedDepth := decrementTraversalDepth(depth)

src/copy.go
@@ -125,16 +125,32 @@ func (g *Commands) copy(src *File, destPath string) (*File, error) {
 		return nil, destErr
 	}
 
-	children := g.rem.findChildren(src.Id, false)
+	pagePair := g.rem.findChildren(src.Id, false)
 
-	for child := range children {
-		// TODO: add concurrency after retry scheme is added
-		// because could suffer from rate limit restrictions
-		chName := sepJoin("/", destPath, child.Name)
-		_, chErr := g.copy(child, chName)
+	children := pagePair.filesChan
+	errsChan := pagePair.errsChan
 
-		if chErr != nil {
-			g.log.LogErrf("copy: %s: %v\n", chName, chErr)
+	working := true
+	for working {
+		select {
+		case err := <-errsChan:
+			if err != nil {
+				// TODO: What should we do here? Should we continue?
+				return nil, err
+			}
+		case child, stillHasContent := <-children:
+			if !stillHasContent {
+				working = false
+				break
+			}
+			// TODO: add concurrency after retry scheme is added
+			// because could suffer from rate limit restrictions
+			chName := sepJoin("/", destPath, child.Name)
+			_, chErr := g.copy(child, chName)
+
+			if chErr != nil {
+				g.log.LogErrf("copy: %s: %v\n", chName, chErr)
+			}
 		}
 	}
 

src/id.go
@@ -35,7 +35,7 @@ func idPrintAndRecurse(g *Commands, parentId, relToRootPath string, depth int) (
 		return
 	}
 
-	children := g.rem.FindByParentId(parentId, g.opts.Hidden)
+	pagePair := g.rem.FindByParentId(parentId, g.opts.Hidden)
 
 	separatorPrefix := relToRootPath
 	if rootLike(separatorPrefix) {
@@ -43,35 +43,81 @@ func idPrintAndRecurse(g *Commands, parentId, relToRootPath string, depth int) (
 		separatorPrefix = ""
 	}
 
-	for child := range children {
-		if child == nil {
-			continue
-		}
-		childRelToRootPath := sepJoin(RemoteSeparator, separatorPrefix, child.Name)
-		cErr := idPrintAndRecurse(g, child.Id, childRelToRootPath, decrementedDepth)
-		if cErr != nil {
-			err = combineErrors(err, cErr)
+	errsChan := pagePair.errsChan
+	children := pagePair.filesChan
+
+	working := true
+	for working {
+		select {
+		case pageErr := <-errsChan:
+			if pageErr != nil {
+				return pageErr
+			}
+		case child, stillHasContent := <-children:
+			if !stillHasContent {
+				working = false
+				break
+			}
+
+			if child == nil {
+				continue
+			}
+			childRelToRootPath := sepJoin(RemoteSeparator, separatorPrefix, child.Name)
+			cErr := idPrintAndRecurse(g, child.Id, childRelToRootPath, decrementedDepth)
+			if cErr != nil {
+				err = combineErrors(err, cErr)
+			}
 		}
 	}
 
 	return err
 }
 
+var idTableHeader = fmt.Sprintf("%*s %s", int(fileIdWidth), "FileId", "Relative Path")
+
 func (g *Commands) Id() (err error) {
-	header := fmt.Sprintf("%*s %s", int(fileIdWidth), "FileId", "Relative Path")
 	headerPrinted := false
 
+	iterCount := uint64(0)
 	for _, relToRootPath := range g.opts.Sources {
-		remotes := g.rem.FindByPathM(relToRootPath)
-		iterCount := uint64(0)
-		for rem := range remotes {
+		ithIterCount, ithHeaderPrinted, ithErr := g.idPrintRecursePerPath(relToRootPath, headerPrinted)
+		if ithErr != nil {
+			err = reComposeError(err, ithErr.Error())
+		}
+		headerPrinted = ithHeaderPrinted
+		iterCount += ithIterCount
+	}
+
+	return err
+}
+
+func (g *Commands) idPrintRecursePerPath(relToRootPath string, headerPrinted bool) (uint64, bool, error) {
+	pagePair := g.rem.FindByPathM(relToRootPath)
+
+	iterCount := uint64(0)
+	remotes := pagePair.filesChan
+	errsChan := pagePair.errsChan
+	var err error
+
+	working := true
+	for working {
+		select {
+		case pageErr := <-errsChan:
+			if pageErr != nil {
+				return iterCount, headerPrinted, pageErr
+			}
+		case rem, stillHasContent := <-remotes:
+			if !stillHasContent {
+				working = false
+				break
+			}
 			if rem == nil {
 				err = reComposeError(err, fmt.Sprintf("%s does not exist remotely", customQuote(relToRootPath)))
 				continue
 			}
 
 			if !headerPrinted {
-				g.log.Logln(header)
+				g.log.Logln(idTableHeader)
 				headerPrinted = true
 			}
 
@@ -81,11 +127,11 @@ func (g *Commands) Id() (err error) {
 				err = combineErrors(err, cErr)
 			}
 		}
-
-		if iterCount < 1 {
-			err = reComposeError(err, fmt.Sprintf("%s not matched", customQuote(relToRootPath)))
-		}
 	}
 
-	return err
+	if iterCount < 1 {
+		err = reComposeError(err, fmt.Sprintf("%s not matched", customQuote(relToRootPath)))
+	}
+
+	return iterCount, headerPrinted, err
 }

src/list.go
@@ -75,38 +75,49 @@ func (g *Commands) ListMatches() error {
 		fuzzyLevel: Like, values: g.opts.Sources, inTrash: inTrash, joiner: Or,
 	})
 
-	matches, err := g.rem.FindMatches(mq)
-	if err != nil {
-		return err
-	}
+	pagePair := g.rem.FindMatches(mq)
 
 	spin := g.playabler()
 	spin.play()
+	defer spin.stop()
 
 	traversalCount := 0
 
-	for match := range matches {
-		if match == nil {
-			continue
-		}
+	matches := pagePair.filesChan
+	errsChan := pagePair.errsChan
 
-		travSt := traversalSt{
-			depth:    g.opts.Depth,
-			file:     match,
-			headPath: g.opts.Path,
-			inTrash:  g.opts.InTrash,
-			mask:     g.opts.TypeMask,
-			sorters:  sorters(g.opts),
-		}
+	working := true
+	for working {
+		select {
+		case err := <-errsChan:
+			if err != nil {
+				return err
+			}
+		case match, stillHasContent := <-matches:
+			if !stillHasContent {
+				working = false
+				break
+			}
+			if match == nil {
+				continue
+			}
 
-		traversalCount += 1
+			travSt := traversalSt{
+				depth:    g.opts.Depth,
+				file:     match,
+				headPath: g.opts.Path,
+				inTrash:  g.opts.InTrash,
+				mask:     g.opts.TypeMask,
+				sorters:  sorters(g.opts),
+			}
 
-		if !g.breadthFirst(travSt, spin) {
-			break
+			traversalCount += 1
+
+			if !g.breadthFirst(travSt, spin) {
+				break
+			}
 		}
 	}
-
-	spin.stop()
 
 	if traversalCount < 1 {
 		g.log.LogErrln("no matches found!")
@@ -244,41 +255,67 @@ func (g *Commands) List(byId bool) error {
 	return nil
 }
 
-func (g *Commands) ListShared() (err error) {
+func (g *Commands) listSharedPerPath(relToRootPath string) ([]*keyValue, error) {
+	pagePair := g.rem.FindByPathShared(relToRootPath)
+	errsChan := pagePair.errsChan
+	sharedRemotes := pagePair.filesChan
+
 	var kvList []*keyValue
 
-	for _, relPath := range g.opts.Sources {
-		sharedRemotes, sErr := g.rem.FindByPathShared(relPath)
-		if sErr != nil {
-			g.log.LogErrf("%v: '%s'\n", sErr, relPath)
-			return sErr
-		}
+	working := true
+	for working {
+		select {
+		case err := <-errsChan:
+			if err != nil {
+				g.log.LogErrf("%v: '%s'\n", err, relToRootPath)
+				return kvList, err
+			}
+		case s, stillHasContent := <-sharedRemotes:
+			if !stillHasContent {
+				working = false
+				break
+			}
 
-		parentPath := g.parentPather(relPath)
+			parentPath := g.parentPather(relToRootPath)
 
-		if remoteRootLike(parentPath) {
-			parentPath = ""
-		}
+			if remoteRootLike(parentPath) {
+				parentPath = ""
+			}
 
-		if rootLike(parentPath) {
-			parentPath = ""
-		}
-
-		for s := range sharedRemotes {
-			if remoteRootLike(s.Name) {
-				s.Name = ""
+			if rootLike(parentPath) {
+				parentPath = ""
 			}
 
 			if s == nil {
 				continue
 			}
 
+			if remoteRootLike(s.Name) {
+				s.Name = ""
+			}
+
 			kvList = append(kvList, &keyValue{key: parentPath, value: s})
 		}
 	}
 
+	return kvList, nil
+}
+
+func (g *Commands) ListShared() (err error) {
 	spin := g.playabler()
 	spin.play()
+	defer spin.stop()
+
+	var kvList []*keyValue
+
+	for _, relPath := range g.opts.Sources {
+		childKvList, err := g.listSharedPerPath(relPath)
+		if err != nil {
+			return err
+		}
+		kvList = append(kvList, childKvList...)
+	}
+
 	for _, kv := range kvList {
 		if kv == nil || kv.value == nil {
 			continue
@@ -392,8 +429,6 @@ func (g *Commands) breadthFirst(travSt traversalSt, spin *playable) bool {
 		canPrompt = g.opts.canPrompt()
 	}
 
-	fileChan := reqDoPage(req, g.opts.Hidden, canPrompt)
-
 	spin.play()
 
 	onlyFiles := nonFolderExplicitly(g.opts.TypeMask)
@@ -402,16 +437,32 @@ func (g *Commands) breadthFirst(travSt traversalSt, spin *playable) bool {
 
 	var collector []*File
 
-	for file := range fileChan {
-		if file == nil {
-			return false
-		}
+	pagePair := reqDoPage(req, g.opts.Hidden, canPrompt)
+	errsChan := pagePair.errsChan
+	filesChan := pagePair.filesChan
 
-		if isHidden(file.Name, g.opts.Hidden) {
-			continue
-		}
+	working := true
+	for working {
+		select {
+		case err := <-errsChan:
+			if err != nil {
+				g.log.LogErrf("%v", err)
+				return false
+			}
+		case file, stillHasContent := <-filesChan:
+			if !stillHasContent {
+				working = false
+				break
+			}
+			if file == nil {
+				return false
+			}
 
-		collector = append(collector, file)
+			if !isHidden(file.Name, g.opts.Hidden) {
+				collector = append(collector, file)
+			}
+
+		}
 	}
 
 	if len(travSt.sorters) >= 1 {

commit 08ef171674e04babd8da74e52aa9fa9aebefbf1f

changes: properly send back nil if no file is found
Ensure that nil is properly sent back after remote
change/file resolution. This regression was caused
by forgetting to send back that nil.

This regression was caused by PR
https://github.com/odeke-em/drive/pull/741.

The consequence of the bug was that trying to push
from non-existent folders would erraneously give
back
```shell
Resolving...
Everything is up-to-date.
```

since the remotesChan channel would get closed
after resolution without sending notifying
whoever was resolving that the file or parent didn't
exist remotely.

Fixes #933

src/remote.go
@@ -1096,8 +1096,14 @@ func (r *Remote) findByPathRecvRawM(parentId string, p []string, trashed bool) *
 					working = false
 					break
 				}
+
 				if f != nil {
 					chanOChan <- r.findByPathRecvRawM(f.Id, rest, trashed)
+				} else {
+					// Ensure that we properly send
+					// back nil even if files were not found.
+					// See https://github.com/odeke-em/drive/issues/933.
+					chanOChan <- wrapInPaginationPair(nil, nil)
 				}
 			}
 		}